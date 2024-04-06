bd10274052de1a95c0da6cef4aaba2b49af6f9a5
mechash <j.mechash@gmail.com>
2024-04-06 14:12:13 +0200

    New backup from 06/04/24 - 14:12:13

--- oprint/lib/python3.7/site-packages/octoprint/plugins/pluginmanager/exceptions.py
+++
@@ -1,6 +0,0 @@
-__license__ = "GNU Affero General Public License http://www.gnu.org/licenses/agpl.html"
-__copyright__ = "Copyright (C) 2020 The OctoPrint Project - Released under terms of the AGPLv3 License"
-
-
-class InvalidPackageFormat(Exception):
-    pass
